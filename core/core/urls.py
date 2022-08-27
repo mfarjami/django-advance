f868af1532d17cd2a7d080becb760ed6983cfeb0
mfarjami <biroghlan95@gmail.com>
2022-08-27 10:11:55 +0300

    Set a views url and template to blog and accounts

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 
