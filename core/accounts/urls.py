52872cc9c64eed3bc6e3f99cecd630d041b23d93
Mohammad <78930739+mfarjami@users.noreply.github.com>
2022-11-25 12:13:57 +0330

    added caching to accounts app

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path('send_email', views.send_email, name='send_email'),
+    path('test', views.test, name='test'),
     # path('api/v1/',include('accounts.api.v1.urls')),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,10 +1,17 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
-from time import sleep
+from django.http import HttpResponse, JsonResponse
 from .tasks import sendemail
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+import requests
 
 # Create your views here.
 
 def send_email(request):
     sendemail.delay()
     return HttpResponse('<h1>send email</h1>')
+
+@cache_page(60)
+def test(request):
+    response = requests.get("https://432aec5b-2450-46fc-ac4f-cb32378717d6.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
